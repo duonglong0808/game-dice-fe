a3a759954d00dda4c3c34ed00fa47f96581aa7b1
duonglong0808 <165501031+duonglong0808@users.noreply.github.com>
2024-05-12 20:39:34 +0700

    update logo and title

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -6,6 +6,18 @@ import { Suspense } from 'react';
 export const metadata: Metadata = {
   title: 'KU Trò chơi Casino',
   description: 'KU Trò chơi Casino top 1 ',
+  icons: [
+    {
+      media: '(prefers-color-scheme: light)',
+      url: '/images/favicon.ico',
+      href: '/images/favicon.ico',
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      url: '/images/favicon.ico',
+      href: '/images/favicon.ico',
+    },
+  ],
 };
 
 export default function Layout({
