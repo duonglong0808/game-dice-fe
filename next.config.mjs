0f8818c7a945fca6c3cb5d59ac2f8e9be08aa4dc
duonglong0808 <165501031+duonglong0808@users.noreply.github.com>
2024-05-31 18:25:32 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,20 @@ const nextConfig = {
     URL_MAIN: 'http://localhost:3002',
   },
 
-  // Production
+  // Dev
   // env: {
   //  URL_MAIN: 'https://vk169.net',
   //   API_URL: 'https://api.vk169.net/api',
   //   API_GAME_DICE: 'https://game.api.vk169.net/api',
   //   API_URL_WSK: 'https://game.wsk.vk169.net',
   //   HOST_KU: 'https://vn.vc3559k.net/api',
+  // },
+   // Production
+  //  env: {
+  //   URL_MAIN: 'https://ku3933d.net',
+  //   API_URL: 'https://api.ku3933d.net/api',
+  //   API_GAME_DICE: 'https://api-game.ku3933d.net/api',
+  //   API_URL_WSK: 'https://ws.ku3933d.net',
   // },
   images: {
     domains: ['storage.googleapis.com'],
